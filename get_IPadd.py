d19a71ecb67fbfafd9358e79621a82f07e21d4fd
ReymartRodas <95275752+ReymartRodas@users.noreply.github.com>
2022-10-26 10:09:52 +0800

    Update get_IPadd.py

--- get_IPadd.py
+++ get_IPadd.py
@@ -4,13 +4,13 @@ import requests
 
 
 
-
+# get the ip of the user#
 def get_ip():
     response = requests.get('https://api.ipify.org?format=json').json()
     return response["ip"]
     
 
-
+#get the information of the user using the ip address"
 def get_location():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
