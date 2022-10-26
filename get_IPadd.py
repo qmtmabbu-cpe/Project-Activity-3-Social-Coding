80ae5e2d5c95efcb2efccadbf6174349da82b24b
ReymartRodas <sample@example.com>
2022-10-26 01:36:56 +0000

    New added code

---
+++ get_IPadd.py
@@ -0,0 +1,52 @@
+from ipaddress import ip_address
+from urllib import response
+import requests
+
+
+
+
+def get_ip():
+    response = requests.get('https://api.ipify.org?format=json').json()
+    return response["ip"]
+    
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name"),
+        "postal": response.get("postal")
+        
+        
+    }
+    print("============================================")
+    print("============================================")
+    print("This is the Ipv4 of the user: " + ip_address)
+    print("============================================")
+    print("============================================")
+    print("This is the City of the user: " + response.get("city"))
+    print("============================================")
+    print("============================================")
+    print("This is the Country of the user: " + response.get("country_name"))
+    print("============================================")
+    print("============================================")
+    print("This is the Postal code of the user: " + response.get("postal"))
+    print("============================================")
+    print("============================================")
+    
+
+
+
+
+    return location_data
+
+
+
+
+
+print(get_location())
+
